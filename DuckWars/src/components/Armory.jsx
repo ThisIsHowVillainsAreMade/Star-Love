b8fe8163604d9f0e0f43e1e50abbd9819688e818
Goodtimenl <gregory.delaloeuf@gmail.com>
2023-12-19 20:44:11 +0100

    Commit for upgrating the Weapon text, prices and description

--- DuckWars/src/components/Armory.jsx
+++ DuckWars/src/components/Armory.jsx
@@ -1,51 +1,56 @@
 import React, { useState } from 'react';
 import '../CSS/Armory.css'
+import image1 from '../assets/Armory/weapon-cyberpunk-star-wars-353260766.png';
+import image2 from '../assets/Armory/weapon-cyberpunk-star-wars-759990936.png';
+import image3 from '../assets/Armory/weapons-cyberpunk-star-wars-407320527.png';
+import image4 from '../assets/Armory/weapons-cyberpunk-star-wars-407320527.png';
+import image5 from '../assets/Armory/weapons-cyberpunk-star-wars-459974752.png';
 
 const Armory = () => {
 	const armes = [
 		{
 			id: 1,
-			nom: "Arme 1",
-			src: "../assets/Armory/weapon-cyberpunk-star-wars-353260766.png",
+			nom: "Neuro-Blaster 3000 ",
+			src: image1,
 			categorie: "Catégorie A",
 			prix: "347 650.783",
-			informations: "Description de l'arme 1",
+			informations: "une arme futuriste qui utilise des impulsions duckologiques pour désorienter l'ennemi.",
 			puissance: "Élevée"
 		},
 		{
 			id: 2,
-			nom: "Arme 2",
-			src: "../assets/Armory/weapon-cyberpunk-star-wars-61708700.png",
+			nom: "Pulse Disruptor MK II",
+			src: image2,
 			categorie: "Catégorie A",
 			prix: "452 090.360",
-			informations: "Description de l'arme 2",
+			informations: "Une arme énergétique capable de perturber les marres électromagnétiques et les ducks électroniques.",
 			puissance: "Élevée"
 		},
 		{
 			id: 3,
-			nom: "Arme 3",
-			src: "../assets/Armory/weapon-cyberpunk-star-wars-759990936.png",
+			nom: "Shadowstrike Coincoin",
+			src: image3,
 			categorie: "Catégorie A",
 			prix: "193 765.009",
-			informations: "Description de l'arme 3",
+			informations: "une arme silencieuse et furtive, idéale pour les assassinats discrets dans un bassin calme ou fréquenté.",
 			puissance: "Élevée"
 		},
 		{
 			id: 4,
-			nom: "Arme 4",
-			src: "../assets/Armory/weapons-cyberpunk-star-wars-407320527.png",
+			nom: "Plasma Cutter X9",
+			src: image4,
 			categorie: "Catégorie B",
 			prix: "867 409.90",
-			informations: "Description de l'arme 4",
+			informations: "une arme puissante qui émet un faisceau de plumes concentré, capable de découper presque tout.",
 			puissance: "Élevée"
 		},
 		{
 			id: 5,
-			nom: "Arme 5",
-			src: "../assets/Armory/weapons-cyberpunk-star-wars-459974752.png",
+			nom: "Cybernetic Railgun ZX",
+			src: image5,
 			categorie: "Catégorie C",
 			prix: "110263.865",
-			informations: "Description de l'arme 5",
+			informations: "un railgun avancé intégrant la technologie duckbernétique pour une précision et une puissance accrues.",
 			puissance: "Élevée"
 		},
 	];
@@ -71,7 +76,8 @@ const Armory = () => {
 					<div className='conTainerDetArm'>
 						<img src={armeActive.src} alt={armeActive.nom} className="image-grande" />
 						<h3>{armeActive.nom}</h3>
-						<p>{armeActive.informations}</p>
+						<p className='weponDesc'>{armeActive.informations}</p>
+						<p className='weaponPrice'>Prix : {armeActive.prix}</p>
 					</div>
 				</div>
 			)}
