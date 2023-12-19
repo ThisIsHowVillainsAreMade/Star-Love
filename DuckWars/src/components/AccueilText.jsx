4512a0043bce1c0024e3dbd019f9316a774568ac
Yohan Soundara <yohan.soundara@gmail.com>
2023-12-19 01:32:09 +0100

    AccueilText defilement

---
+++ DuckWars/src/components/AccueilText.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../CSS/AccueilText.css'
+
+function AccueilText () {
+    return (
+    <div className="accueilContainer">
+    <div className="accueilText">
+    <div className="scroll-text">
+      <h1>STAR WARS</h1>
+      <h2>Scrolling Text Effect</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!</p>
+    </div>
+    </div>
+  </div>)
+}
+export default AccueilText
