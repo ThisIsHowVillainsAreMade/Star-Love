5c44e67aefeb9f0266e77c2579fb944b5aa4d371
Romain <romainbigalion@hotmail.fr>
2023-12-19 10:45:53 +0100

    ajout du modal pour la radio + css attente pour les musiques

--- DuckWars/src/components/Radio.jsx
+++ DuckWars/src/components/Radio.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
+// import Bar from ''
 import '../CSS/Radio.css'
-function Modal({ imageUrl, imgModal }) {
+
+function Modal({ imageUrl, imgModal, musicUrls }) {
   const [displayModal, setDisplayModal] = useState(false);
+  const [mainAudio, setMainAudio] = useState(null);
+  const [siteAudioPaused, setSiteAudioPaused] = useState(false);
+
+  useEffect(() => {
+    const audio = new Audio('chemin/vers/votre/musique-sur-le-site.mp3'); //chemin pour la musique principale
+    setMainAudio(audio);
+
+    return () => {
+      audio.pause();
+    };
+  }, []);
 
   const handleImageClick = () => {
+    if (mainAudio) {
+      mainAudio.pause();
+      setSiteAudioPaused(true); // Met en pause la musique principale
+    }
     setDisplayModal(true);
   };
 
   const closeModal = () => {
+    if (mainAudio && siteAudioPaused) {
+      mainAudio.play(); // Rejoue la musique principale
+      setSiteAudioPaused(false);
+    }
     setDisplayModal(false);
   };
 
@@ -22,6 +43,13 @@ function Modal({ imageUrl, imgModal }) {
               &times;
             </span>
             <img src={imgModal} alt="Bouton radio" className="modal-image" />
+            <div className="audio-container">
+              {musicUrls.map((musicUrl, index) => (
+                <audio key={index} controls autoPlay className="audio-player">
+                  <source src={musicUrl} type="audio/mpeg" />
+                </audio>
+              ))}
+            </div>
           </div>
         </div>
       )}
