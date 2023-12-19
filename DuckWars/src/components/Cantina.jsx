3c90dbab456d5851e73848f34546de83d1fb20ea
cedric chemin <cedric.chemin33@gmail.com>
2023-12-19 16:25:24 +0100

    finalisation Bar

--- DuckWars/src/components/Cantina.jsx
+++ DuckWars/src/components/Cantina.jsx
@@ -5,29 +5,14 @@ function Cantina () {
     console.log("Cantina");
 
     return (
-    <div className="Cantina">
-        <div className="menus">
-        <div className = "wookieSexual">
-            <img src="public/wookie.png" alt="wookie"/>
-        </div>
-        <div className = "Vente">
-                <img src="public/canard.png" alt="foieGras" />
-        </div>
-        <div className = "VenteArmes">
-            <img src="public/armes.png" alt="armes" />
-        </div>
-        <div className = "JetCanard">
-            <img src="public/jet duck.png" alt="jetCanard" />
-        </div>
-        <div className = "Combat">
-            <img src="public/combat Duck.png" alt="combat duck" />
-        </div>
-        <div className = "Radio">
-            <img src="public/radio.png" alt="radio" />
-        </div>
-        </div>
-    </div>
-     
+    <div className="Cantina" >
+            <img className="Background" src="public/back-no-element.png" alt="marché"/>
+            <img className="wookieSexual" src="public/string.png" alt="wookie"/>
+            <img className = "VenteArmes" src="public/weapons-bar.png" alt="armes" />
+            <img className="Radio" src="public/radio.png" alt="radio" />
+            <img className="drug" src="public/drug.png" alt="drug" />
+            <img className="Wanted" src="public/wanted-screen.png" alt="Wanted" />
+    </div> 
     )
 }
 
