79599ed9aecfd3d0409a3892677c98ddbb1b2887
Romain <romainbigalion@hotmail.fr>
2023-12-19 16:05:00 +0100

    ajout des modifications modal

--- DuckWars/src/components/Modal.jsx
+++ DuckWars/src/components/Modal.jsx
@@ -1,36 +1,74 @@
-import '../CSS/Modal.css'
-
-
+import "../CSS/Modal.css";
 
 const Modal = ({ displayModal, closeModal, image, text }) => {
   const divStyle = {
-    display: displayModal ? 'block' : 'none',
+    display: displayModal ? "block" : "none",
   };
-//   vérifie la valeur de modale
+  //   vérifie la valeur de la modal si elle est vrai affiche en block sinon display en none
 
   const handleCloseModal = () => {
     closeModal();
   };
-  //ferme le modale
+  //constante pour fermer la modal
 
   const handleCloseButtonClick = (e) => {
     e.stopPropagation();
     handleCloseModal();
   };
-  //idem mais sur le bouton fermeture + empeche la propagation du clic sur le bouton fermé + appel de la fonction
+  //idem mais sur le bouton fermeture + empeche la propagation du clic sur le contenu + appel de la fonction pour fermer la modal vu au dessus
 
   return (
     <div className="modal" onClick={handleCloseModal} style={divStyle}>
+      {/* logique d'affichage de la modal en fonction du display  */}
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {/* permet de ne pas fermer la modal si on clique sur le contenu ne remonte pas vers la classname modal*/}
         <span className="close" onClick={handleCloseButtonClick}>
           &times;
+          {/*au clique déclenche la fonction handleCloseButtonClick  donc la fermeture */}
         </span>
         <img src={image} alt="Modal Content" />
         <p>{text}</p>
+        {/* affichage du text et de l'image dans la modal */}
       </div>
     </div>
   );
 };
 
-export default Modal;
+// export default Modal;
 
+// import './App.css'
+// import React, { useState } from 'react';
+// import Modal from './components/Modal'
+
+// function App() {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const imageUrl = '';
+//   const modalText = '';
+
+//   const openModal = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   return (
+//     <div>
+//       <img
+//         src='https://www.pokepedia.fr/images/thumb/c/cc/Carapuce-RFVF.png/800px-Carapuce-RFVF.png'
+//         alt=""
+//         onClick={openModal}
+        
+//       />
+//       <button onClick={openModal}>test</button>
+//       <Modal
+//         displayModal={isModalOpen}
+//         closeModal={closeModal}
+//         image={imageUrl}
+//         text={modalText}
+//       />
+//     </div>
+//   );
+// }
+// export default App;
