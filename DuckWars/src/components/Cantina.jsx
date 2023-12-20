1f3f811a736f76f8f44b9749e3ca1aaf90941758
Florian <f.schaessens@gmail.com>
2023-12-20 01:17:17 +0100

    add drugs and music

--- DuckWars/src/components/Cantina.jsx
+++ DuckWars/src/components/Cantina.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import '../CSS/Cantina.css'
+import '../CSS/Cantina.css';
+import '../CSS/DuckDrogs.css';
+import {useAudio} from "./BackgroundMusic"
 
-function Cantina () {
-    console.log("Cantina");
+const startEffect = () => {
+    document.body.classList.add('animatedComplexInvert');
+    setTimeout(() => {
+        document.body.classList.add('blinking');
+    }, 4000);
+
+    setTimeout(() => {
+        document.body.classList.remove('animatedComplexInvert');
+        document.body.classList.remove('blinking');
+    }, 8000);
+};
+
+
+function Cantina() {
+    const [playing, togglePlayback] = useAudio("/stranger-things-124008.mp3");
 
     return (
-    <div className="Cantina" >
+        <div className="Cantina" >
             <img className="Background" src="/back-no-element.png" alt="marché"/>
             <img className="wookieSexual" src="/string.png" alt="wookie"/>
-            <img className = "VenteArmes" src="/weapons-bar.png" alt="armes" />
-            <img className="Radio" src="/radio.png" alt="radio" />
-            <img className="drug" src="/drug.png" alt="drug" />
+            <img className="VenteArmes" src="/weapons-bar.png" alt="armes" />
+            <img
+                className="Radio"
+                src="/radio.png"
+                alt="radio"
+                onClick={togglePlayback} 
+            />
+            <img className="drug" src="/drug.png" alt="drug" onClick={startEffect} />
             <img className="Wanted" src="/wanted-screen.png" alt="Wanted" />
             <img className="neon" src="/DuckSociety-Neon.png" alt="neon" />
-    </div> 
-    )
+        </div> 
+    );
 }
 
 export default Cantina;
